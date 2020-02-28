93ce3bfd6a3aafc68e153509bcccc9513d314c6b
ronan <ronan.jahier@orange.fr>
2020-02-28 18:22:21 +0100

    ajout prism

--- js/date.js
+++ js/date.js
@@ -1,7 +1,7 @@
 "use strict";
 
 let mois = ["janvier", "février", "mars", "avril", "mai", "juin", "juillet", "aout", "septembre", "octobre", "novembre", "décembre"];
-let jours = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Venrdedi", "Samedi", "Dimanche"];
+let jours = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"];
 let d = new Date();
 let j = d.getDay();
 let jj = d.getDate();
