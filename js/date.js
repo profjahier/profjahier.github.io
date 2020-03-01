88a5ff1ceabb9a991cfb02beb78d580d6ccf8193
ronan <ronan.jahier@orange.fr>
2020-03-01 12:51:48 +0100

    maj date

--- js/date.js
+++ js/date.js
@@ -1,11 +1,11 @@
 "use strict";
 
 let mois = ["janvier", "février", "mars", "avril", "mai", "juin", "juillet", "aout", "septembre", "octobre", "novembre", "décembre"];
-let jours = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"];
+let jours = ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi"];
 let d = new Date();
 let j = d.getDay();
 let jj = d.getDate();
 let m = d.getMonth();
 
 let date = document.getElementById("date");
-date.textContent = jours[j-1] + ' ' + jj + ' ' + mois[m];
+date.textContent = jours[j] + ' ' + jj + ' ' + mois[m];
