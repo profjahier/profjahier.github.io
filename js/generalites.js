cac778e5cd6f4de45d4b4c670f95a201d71e7905
profjahier <ronan.jahier@orange.fr>
2020-04-13 21:59:40 +0200

    test navigation

--- js/generalites.js
+++ js/generalites.js
@@ -8,3 +8,37 @@ for (let a of liens){
   }
 }
 */  
+
+function menu_page(){
+  let titres = document.getElementsByTagName('h2');
+  let menu = document.getElementById('menu_page');
+  liste = document.createElement('ol');
+  menu.appendChild(liste);
+  for (let i in titres){
+    let titre = titres[i].textContent;
+    titres[i].setAttribute('id', titre+i);
+    item = document.createElement('li');
+    lien = document.createElement('a');
+    lien.setAttribute('href', '#'+titre+i);
+    lien.setAttribute('onclick', 'efface_menu()');
+    lien.textContent = titre;
+    item.appendChild(lien);
+    liste.appendChild(item);
+  }
+}
+
+function efface_menu(){
+  let menu = document.getElementById('menu_page');
+  menu.innerHTML = "";
+}
+
+
+function fleche_up(){
+  let titres = document.getElementsByTagName('h2');
+  for (let i in titres){
+    let titre = titres[i].textContent;
+    titres[i].innerHTML = "<a href='#'><img src='../../img/up.png' alt='retour haut de page' height='20'/></a> " + titre;
+  }
+}
+
+fleche_up();
