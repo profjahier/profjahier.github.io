3c48437917894d26887b3c1101aec52abbdb039b
profjahier <ronan.jahier@orange.fr>
2020-04-14 09:25:05 +0200

    integration menu navigation

--- js/generalites.js
+++ js/generalites.js
@@ -9,30 +9,44 @@ for (let a of liens){
 }
 */  
 
-function menu_page(){
-  let titres = document.getElementsByTagName('h2');
-  let menu = document.getElementById('menu_page');
-  liste = document.createElement('ol');
-  menu.appendChild(liste);
-  for (let i in titres){
-    let titre = titres[i].textContent;
-    titres[i].setAttribute('id', titre+i);
-    item = document.createElement('li');
-    lien = document.createElement('a');
-    lien.setAttribute('href', '#'+titre+i);
-    lien.setAttribute('onclick', 'efface_menu()');
-    lien.textContent = titre;
-    item.appendChild(lien);
-    liste.appendChild(item);
+// ********************************************************************************************************************
+// Crée un menu de navigation automatique qui est inséré parès le titre h1 de chaque page automatiquement 
+let navHTML = ' <nav><a href="#" onclick="creer_menu_page()"><img src="../../img/sommaire.png" alt="Menu Sous-Site" height="30"></a> <a href="index.html"><img src="../../img/racine.png" alt="Menu Sous-Site" height="30"></a> <a href="../../index.html"><img src="../../img/sortie.png" alt="Menu profjahier" height="30"></p><div id="menu_page"></div></div></nav>';
+document.querySelector('h1').insertAdjacentHTML('afterend', navHTML);
+
+// ********************************************************************************************************************
+// Crée un menu de la page en cours (liste des titres h2) si clic sur icone de menu
+let menu_on = false;
+
+function creer_menu_page(){
+  if (!menu_on){
+    let titres = document.getElementsByTagName('h2');
+    let menu = document.getElementById('menu_page');
+    liste = document.createElement('ol');
+    menu.appendChild(liste);
+    for (let i=0; i<titres.length; i++){
+      let titre = titres[i].textContent;
+      titres[i].setAttribute('id', titre+i);
+      item = document.createElement('li');
+      lien = document.createElement('a');
+      lien.setAttribute('href', '#'+titre+i);
+      lien.setAttribute('onclick', 'effacer_menu()');
+      lien.textContent = titre;
+      item.appendChild(lien);
+      liste.appendChild(item);
+    }
+    menu_on = true;
   }
 }
 
-function efface_menu(){
+function effacer_menu(){ // Efface le menu dès qu'on clique sur un lien
   let menu = document.getElementById('menu_page');
   menu.innerHTML = "";
+  menu_on = false;
 }
 
-
+// ********************************************************************************************************************
+// Ajoute une fleche de retour en haut de page devant chaque titre h2
 function fleche_up(){
   let titres = document.getElementsByTagName('h2');
   for (let i in titres){
@@ -42,3 +56,5 @@ function fleche_up(){
 }
 
 fleche_up();
+
+// ********************************************************************************************************************
--- js/mathjax_config_load.js
+++ js/mathjax_config_load.js
@@ -1,40 +1,10 @@
 // Définir ci-dessous la configuration spécifique désirée
-/*
-window.MathJax = {
-  loader: {
-      load: ['[tex]/mchem']
-  },
-  tex: {
-    packages: {'[+]': ['mchem']}
-  }
-};
-*/
-
-/*
-window.MathJax = {
-  loader: {load: ['[tex]/mhchem']},
-  tex: {packages: {'[+]': ['mhchem']}}
-};
-
-window.MathJax = {
-  loader: {load: ['[tex]/physics']},
-  tex: {packages: {'[+]': ['physics']}}
-};
-
-window.MathJax = {
-  loader: {load: ['[tex]/ams']},
-  tex: {packages: {'[+]': ['ams']}}
-};
-*/
-
 window.MathJax = {
   loader: {load: ['[tex]/mhchem', '[tex]/physics', '[tex]/ams']},
   tex: {packages: {'[+]': ['mhchem', 'physics', 'ams']}, }
 };
 
-
-
-
+// fonction qui charge la librairie MathJax automatiquement
 (function () {
   var script = document.createElement('script');
   script.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-chtml.js'; // choisir ici la configuration par défaut à charger
